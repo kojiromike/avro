8913f9bc13503f6dd0d9c2a62d53bc23d2948046
Michael A. Smith <michaels@syapse.com>
2019-06-23 15:49:46 -0400

    AVRO-2410: PEP8 PY3 E703

--- lang/py3/avro/io.py
+++ lang/py3/avro/io.py
@@ -249,7 +249,7 @@ class BinaryDecoder(object):
       raise exn
 
   def check_crc32(self, bytes):
-    checksum = STRUCT_CRC32.unpack(self.read(4))[0];
+    checksum = STRUCT_CRC32.unpack(self.read(4))[0]
     if binascii.crc32(bytes) & 0xffffffff != checksum:
       raise schema.AvroException("Checksum failure")
 
@@ -331,7 +331,7 @@ class BinaryEncoder(object):
     """
     int and long values are written using variable-length, zig-zag coding.
     """
-    self.write_long(datum);
+    self.write_long(datum)
 
   def write_long(self, datum):
     """
@@ -378,7 +378,7 @@ class BinaryEncoder(object):
     """
     A 4-byte, big-endian CRC32 checksum
     """
-    self.write(STRUCT_CRC32.pack(binascii.crc32(bytes) & 0xffffffff));
+    self.write(STRUCT_CRC32.pack(binascii.crc32(bytes) & 0xffffffff))
 
 
 # ------------------------------------------------------------------------------
