f4a42cb40a5cf7e9506ea13be0b62d6c6f9af8ae
Michael A. Smith <michael@smith-li.com>
2019-09-30 19:06:13 -0400

    Use Modern setup.cfg in py2

--- lang/py/setup.py
+++ lang/py/setup.py
@@ -7,14 +7,15 @@
 # to you under the Apache License, Version 2.0 (the
 # "License"); you may not use this file except in compliance
 # with the License.  You may obtain a copy of the License at
-# 
+#
 # https://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 import os
 import setuptools
 
@@ -29,24 +30,4 @@ def _get_version():
     return verfile.read().rstrip().replace("-", "+")
 
 
-setuptools.setup(
-  name = 'avro',
-  version = _get_version(),
-  packages = ['avro'],
-  package_dir = {'': 'src'},
-  scripts = ["./scripts/avro"],
-
-  #include_package_data=True,
-  package_data={'avro': ['LICENSE', 'NOTICE']},
-
-  # metadata for upload to PyPI
-  author = 'Apache Avro',
-  author_email = 'dev@avro.apache.org',
-  description = 'Avro is a serialization and RPC framework.',
-  license = 'Apache License 2.0',
-  keywords = 'avro serialization rpc',
-  url = 'https://avro.apache.org/',
-  extras_require = {
-    'snappy': ['python-snappy'],
-  },
-)
+setuptools.setup(version=_get_version())
